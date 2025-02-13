006b36203e9e0400a8136b308fcaa938b4ab09af
ShashiGurav1995 <shashigurav1995@gmail.com>
2025-02-13 12:53:51 +0530

    logo

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -24,9 +24,9 @@ const Login = ()=>{
         <>
         <Header/>
         <div className="absolute">
-        <img src="../../Images/IN-en-20250203-TRIFECTA-perspective_46eb8857-face-4ea6-b901-dbf22b461369_medium.jpg"/>
+        <img src="https://assets.nflxext.com/ffe/siteui/vlv3/f268d374-734d-474f-ad13-af5ba87ef9fc/web/IN-en-20250210-TRIFECTA-perspective_92338d5d-6ccd-4b1a-8536-eb2b0240a55e_large.jpg"/>
         </div>
-        <div className="pt-52">
+        <div className="pt-40">
         <form className="relative z-10 mx-auto max-w-80 w-full bg-black opacity-80 flex justify-center align-middle flex-col p-12 gap-3" onSubmit={(e)=>{e.preventDefault()}}>
             <h1 className="text-white text-4xl">{signUp ? 'Sign Up' : 'Sign In'}</h1>
             {signUp && <input type="text" placeholder="Full Name" className="w-100 py-2 px-1 bg-gray-700 text-gray-100 opacity-100 placeholder:text-gray-300"/>}
