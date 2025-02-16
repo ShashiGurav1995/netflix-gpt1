bce9d86a98864b297403716a6fb12a917e971491
ShashiGurav1995 <shashigurav1995@gmail.com>
2025-02-16 15:53:51 +0530

    authenitication done

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,14 +3,13 @@ import Header from "./Header";
 import { validate } from "../utils/validate";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth } from "../utils/firebase";
-import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
+import { avtar_url, netflix_bg } from "../utils/constants";
 
 const Login = () => {
 
     const [signUp, setSignUp] = useState(false);
     const [errorMessage, setErrorMessage] = useState(null);
-    const navigate = useNavigate();
     const dispatch = useDispatch()
 
     const name = useRef(null);
@@ -21,6 +20,7 @@ const Login = () => {
         return setSignUp(!signUp);
     }
 
+
     const handleBtnClick = () => {
         const message = validate(email.current.value, password.current.value);
         setErrorMessage(message);
@@ -29,39 +29,35 @@ const Login = () => {
 
         if (signUp) {
             createUserWithEmailAndPassword(auth, email.current.value, password.current.value)
-                .then((userCredential) => {
+                .then(async (userCredential) => {
                     // Signed up 
                     const user = userCredential.user;
-                    updateProfile(user, {
-                        displayName: name.current.value, photoURL : 'https://cdn.vectorstock.com/i/1000v/89/50/generic-person-gray-photo-placeholder-man-vector-24848950.avif'
-                      }).then(() => {
-                        const {uid, email, displayName, photoURL} = auth.currentUser;
-                                        dispatch(addUser({uid : uid, email : email, displayName : displayName, photoURL}));
-                      }).catch((error) => {
+                    await updateProfile(user, {
+                        displayName: name.current.value, photoURL: avtar_url
+                    }).then(async () => {
+                        const { uid, email, displayName, photoURL } = auth.currentUser;
+                        await dispatch(addUser({ uid: uid, email: email, displayName: displayName, photoURL: avtar_url }));
+                    }).catch((error) => {
                         // An error occurred
                         // ...
-                      });
-                    navigate("/browse");
+                    });
+
                 })
                 .catch((error) => {
                     const errorCode = error.code;
                     const errorMessage = error.message;
                     setErrorMessage(errorCode + ': ' + errorMessage);
-                    navigate("/");
                 });
         } else {
             signInWithEmailAndPassword(auth, email.current.value, password.current.value)
                 .then((userCredential) => {
                     // Signed in 
                     const user = userCredential.user;
-                    console.log(user);
-                    navigate("/browse");                    
                 })
                 .catch((error) => {
                     const errorCode = error.code;
                     const errorMessage = error.message;
                     setErrorMessage(errorCode + ': ' + errorMessage);
-                    navigate("/");
                 });
         }
     }
@@ -70,7 +66,7 @@ const Login = () => {
         <>
             <Header />
             <div className="absolute">
-                <img src="https://assets.nflxext.com/ffe/siteui/vlv3/f268d374-734d-474f-ad13-af5ba87ef9fc/web/IN-en-20250210-TRIFECTA-perspective_92338d5d-6ccd-4b1a-8536-eb2b0240a55e_large.jpg" />
+                <img src={netflix_bg} />
             </div>
             <div className="pt-40">
                 <form className="relative z-10 mx-auto max-w-80 w-full bg-black opacity-80 flex justify-center align-middle flex-col p-12 gap-3" onSubmit={(e) => { e.preventDefault() }}>
